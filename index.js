3db7bdf80e9a7c356055ddb47e0e3c98ef507efe
Bill Marques <pnxbill@gmail.com>
2020-12-11 17:24:14 -0300

    feat: cleaned main folder, importing necessary functions from functions folder

--- index.js
+++ index.js
@@ -1,80 +1,26 @@
-const puppeteer = require('puppeteer');
-const pdfparse = require('pdf-parse');
-const request = require("request-promise-native");
-const fs = require('fs');
+const cron = require("node-cron");
+const express = require("express");
 
-const pdfExtract = async (filename) => {
-  const pdffile = fs.readFileSync(filename);
-  const data = await pdfparse(pdffile);
+const { getDownloadLink } = require('./functions/getDownloadLink');
+const { pdfExtract } = require('./functions/pdfExtract');
+const { downloadPdf } = require('./functions/downloadPdf');
 
-  const confirmados = Number(data.text.match(/CASOSCONFIRMADOS\s+\d.\d+/gm)[0].trim().replace('CASOSCONFIRMADOS', ''));
+app = express();
+app.listen(8080);
 
-  const recuperados = Number(data.text.match(/CASOS RECUPERADOS\s+\d.\d+/gm)[0].trim().replace('CASOS RECUPERADOS', ''));
+// cron.schedule("0 13 */1 * *", () => {
+//   routine();
+// });
 
-  const monitoramento = Number(data.text.match(/EM MONITORAMENTO\s+\d.\d+/gm)[0].trim().replace('EM MONITORAMENTO', ''));
+cron.schedule("* * * * *", () => {
+  routine();
+});
 
-  const obto_investigacao = Number(data.text.match(/ÓBITOS EM INVESTIGAÇÃO\s+\d+/gm)[0].trim().replace('ÓBITOS EM INVESTIGAÇÃO', ''));
-
-  const obto_descartado = Number(data.text.match(/ÓBITOS DESCARTADOS\s+\d+/gm)[0].trim().replace('ÓBITOS DESCARTADOS', ''));
-
-  const obto_confirmado = Number(data.text.match(/ÓBITO CONFIRMADO\s+\d+/gm)[0].trim().replace('ÓBITO CONFIRMADO', ''));
-
-
-  return {
-    dia: dataAtualFormatada(),
-    confirmados,
-    recuperados,
-    monitoramento,
-    obto_investigacao,
-    obto_descartado,
-    obto_confirmado
-  }
-}
-
-function dataAtualFormatada() {
-  var data = new Date(),
-    dia = data.getDate().toString(),
-    diaF = (dia.length == 1) ? '0' + dia : dia,
-    mes = (data.getMonth() + 1).toString(), //+1 pois no getMonth Janeiro começa com zero.
-    mesF = (mes.length == 1) ? '0' + mes : mes,
-    anoF = data.getFullYear();
-  return diaF + "/" + mesF + "/" + anoF;
-}
-
-
-async function downloadPDF(outputFilename, pdfURL) {
-  let pdfBuffer = await request.get({ uri: pdfURL, encoding: null, strictSSL: false });
-  console.log("Writing downloaded PDF file to " + outputFilename + "...");
-  fs.writeFileSync(outputFilename, pdfBuffer);
-}
-
-const getData = async () => {
-  const url = 'http://www.congonhas.mg.gov.br/index.php/atualizacao-informe-epidemiologico/';
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(url)
-
-  const result = await page.evaluate(() => {
-    let link;
-    document.querySelectorAll('a')
-      .forEach((a) => {
-        if (a.innerText.includes('Informe Epidemiológico'))
-          link = {
-            href: a.href,
-            date: a.innerText.slice(-5)
-          }
-      })
-    return link
-  })
-  browser.close();
-  return result;
-}
-
-(async () => {
-  const { href, date } = await getData();
+const routine = async () => {
+  const { href, date } = await getDownloadLink();
   const filename = `Informe ${date}.pdf`
-  await downloadPDF(filename, href)
+  await downloadPdf(filename, href)
   // const filename = `Informe ${`02-12`}.pdf`
   const dailyInfo = await pdfExtract(filename)
   console.log(dailyInfo)
-})();
+};
