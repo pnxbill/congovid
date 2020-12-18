15e74624bcebd8aaa2bfbfacb3dd30ac8f9fcc28
Bill Marques <pnxbill@gmail.com>
2020-12-18 13:40:54 -0300

    separeted file for scheduler to work with heroku

---
+++ src/schedule.js
@@ -0,0 +1,27 @@
+const { downloadPdf } = require("./functions/downloadPdf");
+const { getDownloadLink } = require("./functions/getDownloadLink");
+const { pdfExtract } = require("./functions/pdfExtract");
+const { resolvers } = require('./database/resolvers/informeResolver');
+const fs = require('fs');
+
+const routine = async () => {
+  const { href, date } = await getDownloadLink();
+  const filename = `Informe ${date}.pdf`
+  await downloadPdf(filename, href)
+  // const filename = `Informe ${`02-12`}.pdf`
+  // const dailyInfo = await pdfExtract(filename)
+  const daily = {
+    ...await pdfExtract(filename),
+    href
+  }
+  await resolvers.Mutation.addInforme('any', daily)
+  console.log(daily)
+  try {
+    fs.unlinkSync(filename)
+    //file removed
+  } catch(err) {
+    console.error(err)
+  }
+};
+
+routine();
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const cron = require("node-cron");
-const fs = require('fs');
+// const cron = require("node-cron");
+// const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
 const { typeDefs } = require('./database/schemas/informeSchema');
@@ -9,9 +9,9 @@ const { ApolloServer } = require('apollo-server-express');
 const config = require('./database/connection')
 const routes = require('./routes');
 
-const { getDownloadLink } = require('./functions/getDownloadLink');
-const { pdfExtract } = require('./functions/pdfExtract');
-const { downloadPdf } = require('./functions/downloadPdf');
+// const { getDownloadLink } = require('./functions/getDownloadLink');
+// const { pdfExtract } = require('./functions/pdfExtract');
+// const { downloadPdf } = require('./functions/downloadPdf');
 
 const connect = mongoose.connect(config.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 connect.then((db) => {
@@ -37,30 +37,30 @@ app.listen({ port: PORT }, () =>
 
 
 
-cron.schedule("0 14 */1 * *", () => {
-  routine();
-}, {scheduled : true, timezone: "America/Sao_Paulo"});
+// cron.schedule("0 14 */1 * *", () => {
+//   routine();
+// }, {scheduled : true, timezone: "America/Sao_Paulo"});
 
 // cron.schedule("* * * * *", () => {
 //   routine();
 // });
 
-const routine = async () => {
-  const { href, date } = await getDownloadLink();
-  const filename = `Informe ${date}.pdf`
-  await downloadPdf(filename, href)
-  // const filename = `Informe ${`02-12`}.pdf`
-  // const dailyInfo = await pdfExtract(filename)
-  const daily = {
-    ...await pdfExtract(filename),
-    href
-  }
-  await resolvers.Mutation.addInforme('any', daily)
-  console.log(daily)
-  try {
-    fs.unlinkSync(filename)
-    //file removed
-  } catch(err) {
-    console.error(err)
-  }
-};
+// const routine = async () => {
+//   const { href, date } = await getDownloadLink();
+//   const filename = `Informe ${date}.pdf`
+//   await downloadPdf(filename, href)
+//   // const filename = `Informe ${`02-12`}.pdf`
+//   // const dailyInfo = await pdfExtract(filename)
+//   const daily = {
+//     ...await pdfExtract(filename),
+//     href
+//   }
+//   await resolvers.Mutation.addInforme('any', daily)
+//   console.log(daily)
+//   try {
+//     fs.unlinkSync(filename)
+//     //file removed
+//   } catch(err) {
+//     console.error(err)
+//   }
+// };
