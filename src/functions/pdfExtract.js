1bc62704907a01a898e448559754d3bd6f48affa
Bill Marques <pnxbill@gmail.com>
2020-12-19 14:45:09 -0300

    feat/fix: refactor code inside pdfExtract to be scalable, added . removal from number

--- src/functions/pdfExtract.js
+++ src/functions/pdfExtract.js
@@ -1,41 +1,69 @@
-const pdfparse = require('pdf-parse');
-const fs = require('fs');
+const pdfparse = require("pdf-parse");
+const fs = require("fs");
 
 exports.pdfExtract = async (filename) => {
   const pdffile = fs.readFileSync(filename);
   const data = await pdfparse(pdffile);
 
-  const confirmados = Number(data.text.match(/CASOSCONFIRMADOS\s+\d.\d+/gm)[0].trim().replace('CASOSCONFIRMADOS', ''));
-
-  const recuperados = Number(data.text.match(/CASOS RECUPERADOS\s+\d.\d+/gm)[0].trim().replace('CASOS RECUPERADOS', ''));
-
-  const monitoramento = Number(data.text.match(/EM MONITORAMENTO\s+\d.\d+/gm)[0].trim().replace('EM MONITORAMENTO', ''));
-
-  const obto_investigacao = Number(data.text.match(/ÓBITOS EM INVESTIGAÇÃO\s+\d+/gm)[0].trim().replace('ÓBITOS EM INVESTIGAÇÃO', ''));
-
-  const obto_descartado = Number(data.text.match(/ÓBITOS DESCARTADOS\s+\d+/gm)[0].trim().replace('ÓBITOS DESCARTADOS', ''));
-
-  const obto_confirmado = Number(data.text.match(/ÓBITO CONFIRMADO\s+\d+/gm)[0].trim().replace('ÓBITO CONFIRMADO', ''));
-
+  const scrape = () => {
+    let obj = {};
+    scraped.forEach((el) => {
+      const numb = data.text
+        .match(el.regex)[0]
+        .trim()
+        .replace(el.removed, "")
+        .replace(/[.]/g, "");
+      console.log(numb);
+      obj[el.name] = Number(numb);
+    });
+    return obj;
+  };
 
   return {
     dia: dataAtualFormatada(),
-    confirmados,
-    recuperados,
-    monitoramento,
-    obto_investigacao,
-    obto_descartado,
-    obto_confirmado
-  }
-
-}
+    ...scrape(),
+  };
+};
 
 function dataAtualFormatada() {
   var data = new Date(),
     dia = data.getDate().toString(),
-    diaF = (dia.length == 1) ? '0' + dia : dia,
+    diaF = dia.length == 1 ? "0" + dia : dia,
     mes = (data.getMonth() + 1).toString(), //+1 pois no getMonth Janeiro começa com zero.
-    mesF = (mes.length == 1) ? '0' + mes : mes,
+    mesF = mes.length == 1 ? "0" + mes : mes,
     anoF = data.getFullYear();
   return diaF + "/" + mesF + "/" + anoF;
 }
+
+const scraped = [
+  {
+    name: "confirmados",
+    regex: /CASOSCONFIRMADOS\s+\d.\d+/gm,
+    removed: "CASOSCONFIRMADOS",
+  },
+  {
+    name: "recuperados",
+    regex: /CASOS RECUPERADOS\s+\d.\d+/gm,
+    removed: "CASOS RECUPERADOS",
+  },
+  {
+    name: "monitoramento",
+    regex: /EM MONITORAMENTO\s+\d.\d+/gm,
+    removed: "EM MONITORAMENTO",
+  },
+  {
+    name: "obto_investigacao",
+    regex: /ÓBITOS EM INVESTIGAÇÃO\s+\d+/gm,
+    removed: "ÓBITOS EM INVESTIGAÇÃO",
+  },
+  {
+    name: "obto_descartado",
+    regex: /ÓBITOS DESCARTADOS\s+\d+/gm,
+    removed: "ÓBITOS DESCARTADOS",
+  },
+  {
+    name: "obto_confirmado",
+    regex: /ÓBITO CONFIRMADO\s+\d+/gm,
+    removed: "ÓBITO CONFIRMADO",
+  },
+];
