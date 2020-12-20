dc6a7c3b58b9ec54a58f209ceb2a005f70214e3d
Bill Marques <pnxbill@gmail.com>
2020-12-20 00:12:12 -0300

    added new front and fixing db

--- src/database/models/Informe.js
+++ src/database/models/Informe.js
@@ -1,42 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const informeSchema = new Schema({
-  dia: {
+const informeSchema = new Schema(
+  {
+    dia: {
+      type: Date,
+      required: true,
+    },
+    confirmados: {
+      type: Number,
+      required: true,
+    },
+    recuperados: {
+      type: Number,
+      required: true,
+    },
+    monitoramento: {
+      type: Number,
+      required: true,
+    },
+    obto_investigacao: {
+      type: Number,
+      required: true,
+    },
+    obto_descartado: {
+      type: Number,
+      required: true,
+    },
+    obto_confirmado: {
+      type: Number,
+      required: true,
+    },
+    href: {
       type: String,
-      required: true
+      required: true,
+    },
   },
-  confirmados: {
-      type: Number,
-      required: true
-  },
-  recuperados: {
-      type: Number,
-      required: true
-  },
-  monitoramento: {
-      type: Number,
-      required: true
-  },
-  obto_investigacao: {
-      type: Number,
-      required: true
-  },
-  obto_descartado: {
-      type: Number,
-      required: true
-  },
-  obto_confirmado: {
-      type: Number,
-      required: true
-  },
-  href: {
-      type: String,
-      required: true
-  },
+  {
+    timestamps: true,
+  }
+);
 
-}, {
-    timestamps: true
-});
-
-const Informe = mongoose.model('Informe', informeSchema);
-module.exports = {Informe, informeSchema};
+const Informe = mongoose.model("Informe", informeSchema);
+module.exports = { Informe, informeSchema };
